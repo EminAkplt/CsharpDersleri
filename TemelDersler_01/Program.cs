9dc932885cb888c6431206506a05a612c11c0472
Mehmet Emin AKPOLAT <AkpolatEmin01@gmail.com>
2025-08-01 15:46:36 +0300

    İlk commit

---
+++ TemelDersler_01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemelDersler_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba, C# Programlama Dili!");
+
+
+
+
+            Console.Read()
+        }
+    }
+}
